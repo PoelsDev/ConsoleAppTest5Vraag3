91dd9e3ec7658340ca9a6b2e95a4261171b54851
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:31:42 +0200

    Program

--- ConsoleAppTest5Vraag3/Program.cs
+++ ConsoleAppTest5Vraag3/Program.cs
@@ -9,6 +9,22 @@ namespace ConsoleAppTest5Vraag3
     {
         static void Main(string[] args)
         {
-        }
+            string invoer = "2018-01-19";
+            //string invoer = "Het is vandaag 19 januari.";
+
+            bool succes;
+            DateTime outputdatum = new DateTime();
+            
+            succes = DateTime.TryParse(invoer, out outputdatum);
+
+            if (!succes)
+            {
+                Console.WriteLine("Er werd geen datum herkend.");
+            }
+            else
+            {
+                Console.WriteLine("Het is vandaag {0}.", outputdatum.DayOfWeek);
+            }                                   
+        }       
     }
 }
